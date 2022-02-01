5967d019efe27d9bda6314e485f7377521f11893
davutbilgic <bs.davutbilgic@gmail.com>
2022-02-01 18:05:40 +0100

    record1

--- src/test/java/com/appium/tests/TestRunnerAppium.java
+++ src/test/java/com/appium/tests/TestRunnerAppium.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class TestRunnerAppium {
@@ -25,6 +26,10 @@ public class TestRunnerAppium {
             desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "8.1");
             desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
 
+//            //to specify app for testing
+//            //it can be on your computer or somewhere in cloud
+//            desiredCapabilities.setCapability("app","https://cybertek-appium.s3.amazonaws.com/etsy.apk");
+//            driver = new AppiumDriver<>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
             desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "\\etsy.apk");
 
             desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20000);
@@ -56,7 +61,8 @@ public class TestRunnerAppium {
             desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "8.1");
             //name of the device, if it is real device we need to pass UUID parameter
             desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
-
+            //
+            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
             //either you specify app --> //path/to/app.apk
             //or if app is already installed, you need to specify appActivity and appPackage
@@ -65,8 +71,6 @@ public class TestRunnerAppium {
             desiredCapabilities.setCapability("appPackage", "com.android.calculator2");//application's package name
             desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");//app's MainActivity i.e. LAUNCHER activity name
 
-            //
-            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
             desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20000);
 
@@ -107,10 +111,125 @@ public class TestRunnerAppium {
             Assert.assertEquals("20", actualResultText);
 
 
+            //verify 50-35=15
+            MobileElement minus = driver.findElement(MobileBy.AccessibilityId("minus"));
+            getDigit(5).click();
+            getDigit(0).click();
+            minus.click();
+            getDigit(3).click();
+            getDigit(5).click();
+            equals.click();
+
+            actualResultText = result.getText();
+            Assert.assertEquals("15", actualResultText);
+
+
             //close the app at the end
             driver.closeApp();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Test
+    public void test3() {
+        try {
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            //we use android phone
+            desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+            //version of android
+            desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "8.1");
+            //name of the device, if it is real device we need to pass UUID parameter
+            desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+
+            //derste hata vermisti bu yuzden ogretmen bunu ekledi bende hata vermemesine ragmen ekledim. appium versionla ilgili olabilir.
+            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+//            //to specify app for testing
+//            //it can be on your computer or somewhere in cloud
+//            desiredCapabilities.setCapability("app","https://cybertek-appium.s3.amazonaws.com/etsy.apk");
+//            driver = new AppiumDriver<>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
+            desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "\\etsy.apk");
+
+            desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20000);
+
+            driver = new AppiumDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+
+
+            Thread.sleep(5000);
+
+            MobileElement you = driver.findElement(MobileBy.AccessibilityId("You tab, 4 of 5"));
+            you.click();
+            Thread.sleep(2000);
+
+            MobileElement settings = driver.findElement(By.xpath("//*[@text='Settings']"));
+            settings.click();
+            Thread.sleep(2000);
+
+            MobileElement checkbox = driver.findElement(By.id("com.etsy.android:id/settings_checkbox"));
+            checkbox.click();
+            Thread.sleep(3000);
+
+            //verify after click the checkbox it is not selected
+            Assert.assertFalse(driver.findElement(By.id("com.etsy.android:id/settings_checkbox")).isSelected());
+            Thread.sleep(5000);
+
+            //close the app at the end
+            driver.closeApp();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Test
+    public void realPhoneTest() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        desiredCapabilities.setCapability(MobileCapabilityType.VERSION,"11.1");
+        //we used real device, i get this UUID number from terminal with typing "adb devices"
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"B6BUB6TOBMWO9SJB");//UUID number B6BUB6TOBMWO9SJB
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+//        //to specfiy app for testing
+//        //it can be on your computer or somewhere in cloud
+//        desiredCapabilities.setCapability("app","https://cybertek-appium.s3.amazonaws.com/etsy.apk");
+//        driver = new AppiumDriver<>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "\\etsy.apk");
+
+        desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20000);
+
+        driver = new AppiumDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+
+        Thread.sleep(5000);
+
+        MobileElement you = driver.findElement(MobileBy.AccessibilityId("You tab, 4 of 5"));
+        you.click();
+        Thread.sleep(2000);
+
+        MobileElement settings = driver.findElement(By.xpath("//*[@text='Settings']"));
+        settings.click();
+        Thread.sleep(2000);
+
+        MobileElement checkbox = driver.findElement(By.id("com.etsy.android:id/settings_checkbox"));
+        checkbox.click();
+        Thread.sleep(3000);
+
+        //verify after click the checkbox it is not selected
+        Assert.assertFalse(driver.findElement(By.id("com.etsy.android:id/settings_checkbox")).isSelected());
+        Thread.sleep(1000);
+
+
+
+        driver.closeApp();
+
+    }
+
+    //CREATE A METHOD NAMED getDigit THAT IS RETURNING MOBILE ELEMENT OF THE DIGIT THAT YOU PASS AS A PARAMETER
+    public MobileElement getDigit(int digit) {
+        return driver.findElement(By.id("com.android.calculator2:id/digit_" + digit));
     }
 }
